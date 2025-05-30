8b82789477e60eb488fa4b1fa04ee6b45a84630c
Miguel Rosales <miguel_3041240390@utd.edu.mx>
2025-05-29 23:38:25 -0600

    Add files via upload

---
+++ PROGRAMACION_ESTRUCTURADA/U1/3_list/listas.py
@@ -0,0 +1,7 @@
+# Ejemplo 1: Crear una lista de numeros e imprimir el contenido.
+
+# Ejemplo 2: Crear una lista de palabaras y posteriormente buscar la coincidencia de una palabra.
+
+# Ejmplo 3: Añadir elementos a la lista.
+
+# Ejemplo 4: Crear una multidimensional que permita almacenar el nombre y telefono de una agenda.
